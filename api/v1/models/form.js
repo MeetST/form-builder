49c78356c65814edaab3df7c7a9a80f8f7feb89a
Meet Thakkar <meet.thakkar@agileinfoways.com>
2020-11-05 23:07:43 +0530

    created schema for form and user

---
+++ api/v1/models/form.js
@@ -0,0 +1,52 @@
+import mongoose from 'mongoose'
+let ObjectId = mongoose.Schema.Types.ObjectId;
+const connection = require('../db/connection')
+const Schema = mongoose.Schema
+
+var schema = new Schema({
+    user_id: {
+        type: ObjectId,
+        ref: 'tbl_users',
+    },
+    form_name: {
+        type: String
+    },
+    form_desc: {
+        type: String
+    },
+    elements: [{
+        element_type: {
+            type: String
+        },
+        options: {
+            type: [String]
+        },
+        lable: {
+            type: String
+        }
+    }],
+    updated_at: {
+        type: Date,
+        default: new Date()
+    },
+    created_at: {
+        type: Date,
+        default: new Date()
+    }
+}, {
+    collection: 'tbl_forms'
+})
+
+schema.pre('save', function (next) {
+    let url = this
+    url.created_at = url.updated_at = new Date()
+    next()
+})
+
+schema.pre('update', function (next) {
+    let url = this
+    url.updated_at = new Date()
+    next()
+})
+
+module.exports = connection.model(schema.options.collection, schema)
---
+++ api/v1/models/user.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+let ObjectId = mongoose.Schema.Types.ObjectId;
+const connection = require('../db/connection')
+const Schema = mongoose.Schema
+
+var schema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    updated_at: {
+        type: Date,
+        default: new Date()
+    },
+    created_at: {
+        type: Date,
+        default: new Date()
+    }
+}, {
+    collection: 'tbl_users'
+})
+
+schema.pre('save', function (next) {
+    let url = this
+    url.created_at = url.updated_at = new Date()
+    next()
+})
+
+schema.pre('update', function (next) {
+    let url = this
+    url.updated_at = new Date()
+    next()
+})
+
+module.exports = connection.model(schema.options.collection, schema)
