785ad1c8f322818d47ced9f1e8176062d9df7075
Meet Thakkar <meet.thakkar@agileinfoways.com>
2020-11-05 23:10:41 +0530

    created route for apis

--- api/v1/routes/auth.js
+++ api/v1/routes/auth.js
@@ -1,6 +1,9 @@
 import express from 'express'
 const router = express.Router()
+import FormController from '../controllers/form'
 
+router.post('/api/form/add', FormController.addForm);
+router.get('/api/form/list', FormController.getAllForms);
 
 // exports router
 module.exports = router
